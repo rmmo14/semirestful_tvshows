efe75c021495228c3715a58ec60d970381f14ffa
Rene Montoya <rmontoya.we@gmail.com>
2020-11-30 20:36:19 -0800

    so far only have the input form

---
+++ semi_tvshows_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('shows/new', views.index),
+    path('shows/create', views.create),
+]
--- semi_tvshows_app/views.py
+++ semi_tvshows_app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import tvShow
 
 # Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+def create(request):
+    tvShow.objects.create(
+        title = request.POST['title'],
+        network = request.POST['network'],
+        release_date = request.POST['release_date'],
+        description = request.POST['description']
+    )
+    
+    return redirect('/shows/')
