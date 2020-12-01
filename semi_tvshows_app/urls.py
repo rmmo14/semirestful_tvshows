d77a662b7111fe5534308177c88f0ff2bb68b20c
Rene Montoya <rmontoya.we@gmail.com>
2020-11-30 23:37:23 -0800

    finishing assignment

--- semi_tvshows_app/urls.py
+++ semi_tvshows_app/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.redir),
     path('shows/new', views.index),
     path('shows/create', views.create),
+    path('shows', views.allshows),
+    path('shows/<int:current_id>', views.displayshow),
+    path('shows/<int:current_id>/edit', views.edit),
+    path('shows/<int:current_id>/delete', views.delete),
+    path('shows/<int:current_id>/update', views.update),
+
 ]
--- semi_tvshows_app/views.py
+++ semi_tvshows_app/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from .models import tvShow
 
 # Create your views here.
+def redir(request):
+    return redirect('/shows')
+    
 def index(request):
     return render(request, "index.html")
 
@@ -13,4 +16,38 @@ def create(request):
         description = request.POST['description']
     )
     
-    return redirect('/shows/')
+    return redirect('/shows')
+
+def allshows(request):
+    context = {
+        'tv_shows': tvShow.objects.all()
+    }
+    return render(request, "allshows.html", context)
+
+def displayshow(request, current_id):
+    this_show = tvShow.objects.get(id=current_id)
+    context = {
+        'shows': this_show
+    }
+    return render(request, "showshow.html", context)
+
+def edit(request, current_id):
+    this_show = tvShow.objects.get(id=current_id)
+    context = {
+        'shows' : this_show
+    }
+    return render(request, "edit.html", context)
+
+def update(request, current_id):
+    this_show = tvShow.objects.get(id=current_id)
+    this_show.title = request.POST['title']
+    this_show.network = request.POST['network']
+    this_show.release_date = request.POST['release_date']
+    this_show.description = request.POST['description']
+    this_show.save()
+    return redirect('/shows')
+
+def delete (request, current_id):
+    this_show = tvShow.objects.get(id=current_id)
+    this_show.delete()
+    return redirect('/shows')
