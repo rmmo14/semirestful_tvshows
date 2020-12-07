542c95b609e57c2845ae7cef7b4b574f729cd3bd
Rene Montoya <rmontoya.we@gmail.com>
2020-12-06 17:54:36 -0800

    finishing validation, except redirecting to edit page

--- semi_tvshows_app/models.py
+++ semi_tvshows_app/models.py
@@ -1,6 +1,24 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class myManager(models.Manager):
+    def my_validator(self, postData):
+        errors = {}
+        errors['required'] = ""
+        if len(postData['title']) < 2:
+            errors['title'] = "The title should be at least 2 characters"
+            errors['required'] = "All fields required"
+        if len(postData['network']) < 3:
+            errors['network'] = "The network should be at least 3 characters"
+            errors['required'] = "All fields required"
+        if len(postData['description']) < 10:
+            errors['description'] = "The description should be at least 10 characters"
+            errors['required'] = "All fields required"
+        if datetime.strptime(postData['release_date'], '%Y-%m-%d') > datetime.now():
+            errors['release_date'] = 'Release Date should be in the past'
+        return errors
+
 class tvShow(models.Model):
     title = models.CharField(max_length = 255)
     network = models.CharField(max_length = 255)
@@ -8,3 +26,4 @@ class tvShow(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
+    objects = myManager()
--- semi_tvshows_app/views.py
+++ semi_tvshows_app/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 from .models import tvShow
+from django.contrib import messages
 
 # Create your views here.
 def redir(request):
     return redirect('/shows')
-    
+
 def index(request):
     return render(request, "index.html")
 
@@ -15,7 +16,11 @@ def create(request):
         release_date = request.POST['release_date'],
         description = request.POST['description']
     )
-    
+    errors = tvShow.objects.my_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/shows/new')
     return redirect('/shows')
 
 def allshows(request):
@@ -40,10 +45,18 @@ def edit(request, current_id):
 
 def update(request, current_id):
     this_show = tvShow.objects.get(id=current_id)
+    print('my editing', current_id)
     this_show.title = request.POST['title']
     this_show.network = request.POST['network']
     this_show.release_date = request.POST['release_date']
     this_show.description = request.POST['description']
+    errors = tvShow.objects.my_validator(request.POST)
+    print('errors are:', errors)
+    if len(errors) > 0:
+        print('the ID', current_id)
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/shows') #need to redirect this back to the edit screen of that show, where it populates the unchanged data if there are errors in validation
     this_show.save()
     return redirect('/shows')
 
